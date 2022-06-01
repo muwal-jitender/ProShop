12fe3a51de37dba6d95017fd2b94c6c84371f72a
muwal.jitender@gmail.com <muwal.jitender@gmail.com>
2022-06-01 13:13:14 +0530

    Added the product listing and detail page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h1>Welcome to ProShop</h1>;
+  return (
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route exat path="/" element={<HomeScreen />}></Route>
+            <Route path="/product/:id" element={<ProductScreen />}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App;
