0114bb20980c876810b730e385d7e2377d2937be
muwal.jitender@gmail.com <muwal.jitender@gmail.com>
2022-06-09 15:28:41 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,6 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -29,8 +25,20 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/ui/build")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "ui", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 app.use(notFound);
 
 app.use(errorHandler);
